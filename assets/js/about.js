277973c8783d566d527db8608512b41ca5a735a9
Gu21 <bisson_guillaume@yahoo.fr>
2022-01-03 00:04:12 +0100

    mise à jour le 03/01/2022

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,17 +1,1 @@
-const appearanceTitle = document.querySelector(".box-title-home");
-const titleHome = document.querySelector(".titleHome");
-const paragraph = document.querySelector(".pargraph-home");
 
-
-window.addEventListener('load', () => {
-
- 
-    appearanceTitle.style.visibility = "visible"; 
- 
-
-
- 
-
-
-
-});
