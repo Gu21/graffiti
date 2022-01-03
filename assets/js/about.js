6e15743220ce7eb9eb4ae4cd1fadbedbe4dece56
Gu21 <bisson_guillaume@yahoo.fr>
2022-01-03 19:39:04 +0100

    mise à jour le 03/01/2022

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,1 +1,33 @@
+const titleAbout = document.querySelector(".title-about-home");
+const cardHome = document.querySelectorAll(".card");
+console.log(cardHome);
+// Make the image appear on the scroll
+window.addEventListener("scroll", () => {
 
+//   console.log(window.innerHeight);
+//   console.log(window.scrollY);
+//   console.log(document.body.offsetHeight);
+
+  scrollResult =
+    (window.innerHeight + window.scrollY) / document.body.offsetHeight;
+//   console.log(scrollResult);
+
+  // Show the image
+  if (scrollResult > 0.5) {
+    titleAbout.style.opacity = "1";
+    titleAbout.style.transform = "none";
+  };
+
+  if (scrollResult > 0.6) {
+    cardHome[0].style.opacity = "1";
+    cardHome[0].style.transform = "none";
+    setTimeout(() => {
+      cardHome[1].style.opacity = "1";
+      cardHome[1].style.transform = "none";
+    }, 1000);
+    setTimeout(() => {
+        cardHome[2].style.opacity = "1";
+        cardHome[2].style.transform = "none";
+      }, 1500);
+  };
+});
